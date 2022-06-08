1ccea2d2f574f0a55554c2d0674c0608fe8ec2e7
avalonstanley <avalonstanley@hotmail.com>
2022-06-08 14:38:25 -0400

    Final

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+// File name: MyFirstPortfolio
+// Name: Avalon Stanley
+// Student Id: 300 849 734
+// Date: June 6, 2022
+
 var express = require('express');
 var router = express.Router();//invokes function from express
 
-/* GET home page. */
+
 router.get('/', landing);
 router.get('/home', home);
 router.get('/about', about);
@@ -12,35 +17,88 @@ router.get('/contact', contact);
 
 function landing (req, res, next) {
   res.render(
-    'landing', { title: 'Hello.', userName: 'Avalon' });
+    'landing', 
+  {
+    title: 'Landing'
+  });
 };
 
-/* TO DO */
+
 function home (req, res, next) {
   res.render(
-    'home', { title: 'Home Page', userName: 'Avalon' });
+    'home', 
+    { 
+      title: 'Home Page', userName: 'Avalon'
+    });
 };
 
 function about (req, res, next) {
   res.render(
-    'about', { title: 'About', userName: 'Avalon' });
+    'about', 
+    { 
+      title: 'About'
+    });
 };
 
-/* TO DO */
+
 function projects (req, res, next) {
   res.render(
-    'projectsServices', { title: 'Projects', userName: 'Avalon' });
+    'projectsServices', 
+    { 
+      title: 'Projects', 
+      heading: 'Check out some of my latest projects...',
+      div1Link: '#',
+      div1: '/images/PigsFly.pdf',
+      div1Name: 'Client Brief (MKTG)',
+      div1Text: 'Meet with a client from Virgin Mobile, and put together a brief and proposal which I then presented to them. They were incredibly happy, and actually decided to move forward.',
+      div2Link: 'http://studentweb.cencol.ca/astanle5/Protected%20areas%20Assignment%202/index/ProtectedAreas.html',
+      div2: '/images/ProtectedAreasAssignment.JPG',
+      div2Name: 'Protected Areas Assignment',
+      div2Text: 'First semester of school, was given an simple directive to create a website that proved research was conducted on the protected areas of the world. This was my very first website.',
+      div3Link: 'http://studentweb.cencol.ca/astanle5/Assignment%203/ProductPage.html',
+      div3: '/images/CanadaGooseProductPage.JPG',
+      div3Name: 'Canada Goose Product Page',
+      div3Text: 'Another assignment in my first semester was to put together a product page displaying at least 2 products, logo, pop-up windows that have additional photos and a responsive map. ',
+      div4Link: 'http://studentweb.cencol.ca/astanle5/Week11Project/home.html',
+      div4: '/images/RealtorPageProject.JPG',
+      div4Name: "Realtor's Personal Website",
+      div4Text: 'Yet another assignment was given in which I was tasked with creating a logo, branding, and functional website to showcase a realtor (fictional), and his/her listings.'
+    });
 };
 
-/* TO DO */
+
 function services (req, res, next) {
   res.render(
-    'projectsServices', { title: 'Projects', userName: 'Avalon' });
+    'projectsServices', 
+    { 
+      title: 'Services', 
+      heading: "I'm still learning, but can definitely help...",
+      div1Link: '/contact',
+      div1: '/images/webdev.png',
+      div1Name: 'General Programming',
+      div1Text: '',
+      div2Link: '/contact',
+      div2: '/images/webdev1.png',
+      div2Name: 'Web Development',
+      div2Text: '',
+      div3Link: '/contact',
+      div3: '/images/mobile.png',
+      div3Name: 'Mobile Apps',
+      div3Text: '',
+      div4Link: '/contact',
+      div4: '/images/sql.png',
+      div4Name: "SQL Development",
+      div4Text: ''
+    });
 };
 
 function contact (req, res, next) {
   res.render(
-    'contact', { title: 'Projects', heading: 'Give me a shout.' });
+    'contact', 
+    { 
+      title: 'Projects', 
+      heading: 'Give me a shout.' 
+    });
 };
 
 module.exports = router;
